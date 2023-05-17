64191eb3aafeed7c14fdee5ba72a08ea19a88338
alexf <rassylka82@inbox.ru>
2023-05-17 20:33:04 +0300

    If-Else

---
+++ 005_ConditionIfElse/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter your name.");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "masha") {
+    Console.WriteLine("I love you, Masha!");
+} else {
+    Console.WriteLine("Hello, " + username);
+}
