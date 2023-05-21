89e8cc6874ff33a4fc51e5ed3bedc8afc18007eb
alexf <rassylka82@inbox.ru>
2023-05-21 12:21:28 +0300

    Max from 9

---
+++ 016_max_from_9/Program.cs
@@ -0,0 +1,22 @@
+﻿int Max(int arg1, int arg2, int arg3) {
+    int max = arg1;
+    if (arg2 > max) max = arg2;
+    if (arg3 > max) max = arg3;
+    return max;
+}
+
+
+int a1 = 15;
+int b1 = 21;
+int c1 = 39;
+int a2 = 12;
+int b2 = 23;
+int c2 = 33;
+int a3 = 13;
+int b3 = 23;
+int c3 = 33;
+
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+System.Console.WriteLine(Max(max1, max2, max3));
