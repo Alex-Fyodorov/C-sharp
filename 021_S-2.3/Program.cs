76c571fff08685a7d37c67fb18a2418e64ad8570
alexf <rassylka82@inbox.ru>
2023-05-21 15:49:03 +0300

    S-2.3

---
+++ 021_S-2.3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 
+// 7 и 23.
+
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+Console.Clear();
+System.Console.WriteLine("Enter number.");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 7 == 0 && a % 23 == 0) System.Console.WriteLine("yes");
+else System.Console.WriteLine("no");
+
