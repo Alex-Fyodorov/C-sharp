f76a8db1a6ea6ece515c04ab3b6f3fffc204240a
alexf <rassylka82@inbox.ru>
2023-05-21 15:49:35 +0300

    S-2.5

---
+++ 023_S-2.5/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и 
+// показывает наибольшую цифру числа.
+
+// Например:
+//123456 - >13456
+
+Console.Clear();
+System.Console.WriteLine("Enter number");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (num / i != 0) i *= 10;
+int x = num / (i / 10);
+int y = num % (i / 100);
+int result = (i / 100) * x + y;
+System.Console.WriteLine(result);
