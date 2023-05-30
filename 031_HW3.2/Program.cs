fe1d26c133e356f79903fa34580025c2a95d3cf2
alexf <rassylka82@inbox.ru>
2023-05-30 18:49:19 +0300

    hw-3

---
+++ 031_HW3.2/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+//                   ВНИМАНИЕ!
+
+// Поскольку эта задача является частным случаем усложнённой задачи №4, её решение также является упрощённым вариантом задачи №4. Поэтому по большому счёту на этот код можно не смотреть, а переходить к задаче 4.
+
+double[] toFillCoordinates(int size, string point) {
+    double[] coord = new double[size];
+    for (int i = 0; i < size; i++) {
+        bool rightCoord = true;
+        while (rightCoord) {
+            Console.Write($"Введите координату № {i + 1} для {point} точки: ");        
+            try {
+                coord[i] = Convert.ToDouble(Console.ReadLine());  
+                rightCoord = false;                                  
+            }
+            catch {
+                Console.WriteLine("То, что вы ввели, не является числом!");
+            }  
+        }       
+    }
+    return coord;
+}
+
+double distance(double[] a, double[]b) {
+    double sum = 0;
+    for (int i = 0; i < a.Length; i++) {
+        sum += Math.Pow((a[i] - b[i]), 2);
+    }
+    return Math.Sqrt(sum);
+}
+
+Console.Clear();
+System.Console.WriteLine("Данная программа вычисляет расстояние между точками в трёхмерном пространстве.");
+int n = 3;
+System.Console.WriteLine();
+double[] pointA = toFillCoordinates(n, "первой");
+System.Console.WriteLine();
+double[] pointB = toFillCoordinates(n, "второй");
+System.Console.WriteLine();
+System.Console.WriteLine($"Расстояние между введёнными точками равно: {distance(pointA, pointB)}.");
---
+++ 032_HW3.3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int enterNumber() {
+    Console.Clear();    
+    int number = -1;
+    while (number == -1) {
+        System.Console.Write("Введите натуральное число: ");
+        try {
+            number = Convert.ToInt32(Console.ReadLine());  
+            if (number < 1) {
+                System.Console.WriteLine("Введённое вами число не является натуральным.");
+                number = -1;
+            }      
+        }
+        catch {
+            Console.WriteLine("То, что вы ввели, не является целым числом!");
+        }  
+    }     
+    return number;
+}
+
+int n = enterNumber();
+for (int i = 1; i <= n; i++) {
+    System.Console.Write(i * i * i);
+    if (i < n) System.Console.Write(", ");
+}
