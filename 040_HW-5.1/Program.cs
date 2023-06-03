e49b65da7f3f8022b0251a673878a142e6513f1b
alexf <rassylka82@inbox.ru>
2023-06-03 17:59:14 +0300

    hw-5

---
+++ 040_HW-5.1/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int EnterSize()
+{
+    Console.Clear();
+    int number = -1;
+    while (number == -1)
+    {
+        System.Console.Write("Введите размерность массива: ");
+        try
+        {
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number < 1)
+            {
+                System.Console.WriteLine("Введённое вами число не является натуральным.");
+                number = -1;
+            }
+        }
+        catch
+        {
+            Console.WriteLine("То, что вы ввели, не является целым числом!");
+        }
+    }
+    return number;
+}
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write($"{item} ");
+    }
+    System.Console.WriteLine();
+}
+
+void FindEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0) count++;
+    }
+    System.Console.WriteLine($"Количество чётных элементов в массиве: {count}");
+}
+
+Console.Clear();
+int size = EnterSize();
+int[] array = new int[size];
+FillArray(array);
+PrintArray(array);
+FindEven(array);
---
+++ 042_HW-5.3/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 38: Задайте массив вещественных случайных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+int EnterSize()
+{
+    Console.Clear();
+    int number = -1;
+    while (number == -1)
+    {
+        System.Console.Write("Введите размерность массива: ");
+        try
+        {
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number < 1)
+            {
+                System.Console.WriteLine("Введённое вами число не является натуральным.");
+                number = -1;
+            }
+        }
+        catch
+        {
+            Console.WriteLine("То, что вы ввели, не является целым числом!");
+        }
+    }
+    return number;
+}
+
+void FillArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().NextDouble() * 200 - 100;
+    }
+}
+
+void PrintArray(double[] array)
+{
+    foreach (var item in array)
+    {
+        Console.WriteLine(item);
+    }
+    System.Console.WriteLine();
+}
+
+double DiffBetweenMaxAndMin(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array[i];
+        if (array[i] < min) min = array[i];        
+    }
+    return max - min;
+}
+
+Console.Clear();
+int size = EnterSize();
+double[] array = new double[size];
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine($"Разница между максимальным и минимальным элементов массива: {DiffBetweenMaxAndMin(array)}");
