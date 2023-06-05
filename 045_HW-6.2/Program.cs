bba8423cefd50cd381e2d2f28c9e3969269931be
alexf <rassylka82@inbox.ru>
2023-06-05 11:31:09 +0300

    hw-6

---
+++ 045_HW-6.2/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double InputParams(string paramName, int number)
+{
+    double param = 0;
+    bool rightParam = true;
+    while (rightParam)
+    {
+        Console.Write($"Введите параметр {paramName} для {number}-го уравнения: ");
+        try
+        {
+            param = Convert.ToDouble(Console.ReadLine());
+            rightParam = false;
+        }
+        catch
+        {
+            Console.WriteLine("То, что вы ввели, не является числом!");
+        }
+    }    
+    return param;
+}
+
+Console.Clear();
+System.Console.WriteLine("Данная программа вычисляет точку пересечения двух линейных уравнений, представленных видом y = k * x + b.");
+System.Console.WriteLine();
+double[] equation1 = {InputParams("k", 1), InputParams("b", 1)};
+double[] equation2 = {InputParams("k", 2), InputParams("b", 2)};
+System.Console.WriteLine();
+System.Console.WriteLine($"Первое уравнение: y = {equation1[0]} * x + {equation1[1]}.");
+System.Console.WriteLine($"Второе уравнение: y = {equation2[0]} * x + {equation2[1]}.");
+System.Console.WriteLine();
+double x = (equation2[1] - equation1[1]) / (equation1[0] - equation2[0]);
+double y = equation1[0] * x + equation1[1];
+System.Console.WriteLine($"Точка пересечения графиков этих уравнений: ({x}; {y}).");
+
---
+++ 046_HW-6.3/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача НЕГАФИБОНАЧЧИ необязательная. Задайте число. Составьте массив чисел НегаФибоначчи, в том числе для отрицательных индексов.
+// Пример:
+// для k = 9 массив будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+int EnterSize()
+{
+    Console.Clear();
+    int number = -1;
+    while (number == -1)
+    {
+        System.Console.Write("Введите натуральное число: ");
+        try
+        {
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number < 1)
+            {
+                System.Console.WriteLine("Введённое вами число не является натуральным.");
+                number = -1;
+            }
+        }
+        catch
+        {
+            Console.WriteLine("То, что вы ввели, не является целым числом!");
+        }
+    }
+    return number;
+}
+
+void FillArray(int[] array, int size)
+{
+    for (int i = 0; i <= size / 2; i++)
+    {
+        if(i == 0) array[size / 2] = 0;
+        else if(i == 1)
+        {
+            array[size / 2 + 1] = 1;
+            array[size / 2 - 1] = 1;
+        }
+        else
+        {
+            array[size / 2 + i] = array[size / 2 + i - 1] + array[size / 2 + i - 2];
+            array[size / 2 - i] = array[size / 2 - i + 2] - array[size / 2 - i + 1];
+        }
+    }    
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var item in array)
+    {
+        Console.Write($"{item} ");
+    }
+    System.Console.WriteLine();
+}
+
+Console.Clear();
+int size = EnterSize() * 2 - 1;
+int[] array = new int[size];
+FillArray(array, size);
+PrintArray(array);
