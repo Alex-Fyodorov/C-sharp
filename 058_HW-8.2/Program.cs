057579faf2da15211af722da02549fe46208a038
alexf <rassylka82@inbox.ru>
2023-06-11 20:45:22 +0300

    hw-8

---
+++ 058_HW-8.2/Program.cs
@@ -0,0 +1,81 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int EnterNumber(string message)
+{
+    int number = -1;
+    while (number == -1)
+    {
+        System.Console.Write(message);
+        try
+        {
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number < 1)
+            {
+                System.Console.WriteLine("Введённое вами число не является натуральным.");
+                number = -1;
+            }
+        }
+        catch
+        {
+            Console.WriteLine("То, что вы ввели, не является целым числом!");
+        }
+    }
+    return number;
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(-30, 31);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3}    ");
+        System.Console.WriteLine();
+    }
+}
+
+int[] SumOfRows(int[,] array)
+{
+    int[] sum = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            sum[i] += array[i, j];
+    return sum;
+}
+
+int FindMinPosition(int[] array)
+{
+    int min = array[0];
+    int position = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+        {
+            min = array[i];
+            position = i;
+        }
+    }
+    return position;
+}
+
+Console.Clear();
+int rows = EnterNumber("Введите количество строк: ");
+int cols = EnterNumber("Введите количество столбцов: ");
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+System.Console.WriteLine();
+int numberOfRow = FindMinPosition(SumOfRows(array));
+System.Console.WriteLine($"Минимальной суммой элементов обладает {numberOfRow + 1}-я сторока.");
---
+++ 060_HW-8.4/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int EnterNumber(string message)
+{
+    int number = -1;
+    while (number == -1)
+    {
+        System.Console.Write(message);
+        try
+        {
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number < 1)
+            {
+                System.Console.WriteLine("Введённое вами число не является натуральным.");
+                number = -1;
+            }
+        }
+        catch
+        {
+            Console.WriteLine("То, что вы ввели, не является целым числом!");
+        }
+    }
+    return number;
+}
+
+void FillArray(int[,,] array)
+{
+    int[] temp = new int[90];
+    for (int i = 10; i < 100; i++)
+    {
+        temp[i - 10] = i;
+    }
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                int index = new Random().Next(0, 90);
+                while (temp[index] == 0)
+                {
+                    index++;
+                    if (index > 89) index = 0;
+                }
+                array[i, j, k] = temp[index];
+                temp[index] = 0;
+            }
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(2); j++)
+        {
+            for (int k = 0; k < array.GetLength(1); k++)
+            {
+                Console.Write($"{array[i, k, j]}({i}, {k}, {j})  ");
+            }
+            System.Console.WriteLine();
+        }        
+    }
+}
+
+Console.Clear();
+int rows = EnterNumber("Введите количество строк: ");
+int cols = EnterNumber("Введите количество столбцов: ");
+int layers = EnterNumber("Введите количество слоёв: ");
+int[,,] array = new int[rows, cols, layers];
+FillArray(array);
+PrintArray(array);
