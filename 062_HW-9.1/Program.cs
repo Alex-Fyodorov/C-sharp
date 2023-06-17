451ac427830faa133144b626ae683b42819a42eb
alexf <rassylka82@inbox.ru>
2023-06-17 17:11:41 +0300

    hw-9

---
+++ 062_HW-9.1/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int EnterNumber(string message)
+{
+    int number = -1;
+    while (number == -1)
+    {
+        System.Console.Write(message);
+        try
+        {
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number < 1)
+            {
+                System.Console.WriteLine("Введённое вами число не является натуральным.");
+                number = -1;
+            }
+        }
+        catch
+        {
+            Console.WriteLine("То, что вы ввели, не является целым числом!");
+        }
+    }
+    return number;
+}
+
+string StringBuild(int num)
+{
+    if (num == 1) return Convert.ToString(num);
+    return $"{num}, {StringBuild(num - 1)}";
+}
+
+Console.Clear();
+int number = EnterNumber("Введите натуральное число: ");
+System.Console.WriteLine(StringBuild(number));
---
+++ 064_HW-9.3/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int EnterNumber(string message)
+{
+    int number = -1;
+    while (number == -1)
+    {
+        System.Console.Write(message);
+        try
+        {
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number < 0)
+            {
+                System.Console.WriteLine("Введённое вами число является отрицательным.");
+                number = -1;
+            }
+        }
+        catch
+        {
+            Console.WriteLine("То, что вы ввели, не является целым числом!");
+        }
+    }
+    return number;
+}
+
+int AckermannsFunction(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (n == 0) return AckermannsFunction(m - 1, 1);
+    return AckermannsFunction(m - 1, AckermannsFunction(m, n-1));
+}
+
+Console.Clear();
+int number1 = EnterNumber("Введите неотрицательное число №1: ");
+int number2 = EnterNumber("Введите неотрицательное число №2: ");
+System.Console.WriteLine($"A({number1}, {number2}) = {AckermannsFunction(number1, number2)}");
